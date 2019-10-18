0422d3e951d6bec66ba678de566f58afd0da8d30
hegemonies <hegemonies@yandex.ru>
2019-10-18 19:53:48 +0700

    Fix test

--- labs/tests/TestLexer.hpp
+++ labs/tests/TestLexer.hpp
@@ -29,37 +29,46 @@ public:
 
         vector<Token> mockVector;
 
-        mockVector.push_back(Token(TypeToken(CLASS), "class", new Location(3, 1)));
-        mockVector.push_back(Token(TypeToken(ID), "\"Hi\"", new Location(3,7)));
-        mockVector.push_back(Token(TypeToken(L_BRACE), "{", new Location(3,10)));
-        mockVector.push_back(Token(TypeToken(PUBLIC), "public", new Location(4,5)));
-        mockVector.push_back(Token(TypeToken(STATIC), "static", new Location(4,12)));
-        mockVector.push_back(Token(TypeToken(VOID), "void", new Location(4,19)));
-        mockVector.push_back(Token(TypeToken(ID), "main", new Location(4,24)));
-        mockVector.push_back(Token(TypeToken(L_PAREN), "(", new Location(4,28)));
-        mockVector.push_back(Token(TypeToken(STRING), "String", new Location(4,29)));
-        mockVector.push_back(Token(TypeToken(L_SQUARE), "[", new Location(4,35)));
-        mockVector.push_back(Token(TypeToken(R_SQUARE), "]", new Location(4,36)));
-        mockVector.push_back(Token(TypeToken(ID), "args", new Location(4,38)));
-        mockVector.push_back(Token(TypeToken(R_PAREN), ")", new Location(4,42)));
-        mockVector.push_back(Token(TypeToken(L_BRACE), "{", new Location(4,44)));
-        mockVector.push_back(Token(TypeToken(PRINTLN), "System.out.println", new Location(5,9)));
-        mockVector.push_back(Token(TypeToken(L_PAREN), "(", new Location(5,27)));
-        mockVector.push_back(Token(TypeToken(CONST_STRING), "\"Hi\"", new Location(5,28)));
-        mockVector.push_back(Token(TypeToken(R_PAREN), ")", new Location(5,32)));
-        mockVector.push_back(Token(TypeToken(SEMICOLON), ";", new Location(5,33)));
-        mockVector.push_back(Token(TypeToken(R_BRACE), "}", new Location(6,5)));
-        mockVector.push_back(Token(TypeToken(R_BRACE), "}", new Location(7,1)));
+        mockVector.push_back(Token(TypeToken(CLASS), "class", new Location(1, 1)));
+        mockVector.push_back(Token(TypeToken(ID), "Hi", new Location(1,7)));
+        mockVector.push_back(Token(TypeToken(L_BRACE), "{", new Location(1,10)));
+        mockVector.push_back(Token(TypeToken(PUBLIC), "public", new Location(2,13)));
+        mockVector.push_back(Token(TypeToken(STATIC), "static", new Location(2,20)));
+        mockVector.push_back(Token(TypeToken(VOID), "void", new Location(2,27)));
+        mockVector.push_back(Token(TypeToken(ID), "main", new Location(2,32)));
+        mockVector.push_back(Token(TypeToken(L_PAREN), "(", new Location(2,36)));
+        mockVector.push_back(Token(TypeToken(STRING), "String", new Location(2,37)));
+        mockVector.push_back(Token(TypeToken(L_SQUARE), "[", new Location(2,43)));
+        mockVector.push_back(Token(TypeToken(R_SQUARE), "]", new Location(2,44)));
+        mockVector.push_back(Token(TypeToken(ID), "args", new Location(2,46)));
+        mockVector.push_back(Token(TypeToken(R_PAREN), ")", new Location(2,50)));
+        mockVector.push_back(Token(TypeToken(L_BRACE), "{", new Location(2,52)));
+        mockVector.push_back(Token(TypeToken(PRINTLN), "System.out.println", new Location(3,9)));
+        mockVector.push_back(Token(TypeToken(L_PAREN), "(", new Location(3,27)));
+        mockVector.push_back(Token(TypeToken(CONST_STRING), "\"Hi\"", new Location(3,28)));
+        mockVector.push_back(Token(TypeToken(R_PAREN), ")", new Location(3,32)));
+        mockVector.push_back(Token(TypeToken(SEMICOLON), ";", new Location(3,33)));
+        mockVector.push_back(Token(TypeToken(R_BRACE), "}", new Location(4,5)));
+        mockVector.push_back(Token(TypeToken(R_BRACE), "}", new Location(5,1)));
 
 
         ifstream* input_mockFile = new ifstream(file_with_mockdata);
         Lexer lexer(input_mockFile);
         lexer.parseLexer();
-        auto lexerVector = lexer.getTokenVector();
+        vector<Token> lexerVector = lexer.getTokenVector();
+
+        // cout << "Got:\n";
+        // lexer.printLexems();
+
+        // cout << "\n\nExpected:\n";
+        // for (Token token : mockVector) {
+        //     cout << Helper::enumToString(token.type) << "\t\t";
+        //     cout << "\'" << token.value << "\'\t";
+        //     cout << "<" << token.location->toString() << ">" << "\t";
+        //     cout << endl;
+        // }
         
 
-        // TS_ASSERT_EQUALS(equal(lexer.getTokenVector(), mockVector), true);
-        
         for (int i = 0; i < mockVector.size(); ++i) {
             TS_ASSERT(mockVector[i].type == lexerVector[i].type);
             TS_ASSERT(mockVector[i].value == lexerVector[i].value);
